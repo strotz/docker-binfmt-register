e5e75ef2120f8e613b938aa81aec4ed740bd0069
yen3 <yen3rc@gmail.com>
2017-11-19 08:44:18 +0800

    Refine test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
 
-docker build -t yen3/test .
+binfmt_image="yen3/test"
+platform=$(uname)
+mount_option=""
 
-# aarch64
-docker run --privileged --rm yen3/test set aarch64
-docker run --rm yen3/test get aarch64 > qemu-aarch64-static
-chmod +x qemu-aarch64-static
-docker run -it --rm -v $(pwd)/qemu-aarch64-static:/usr/local/bin/qemu-aarch64-static arm64v8/ubuntu uname -a
-docker run --privileged --rm yen3/test clear aarch64
+set_binfmt() {
+    cpu=$1
+    qemu_bin="qemu-${cpu}-static"
 
-# arm
-docker run --privileged --rm yen3/test set arm
-docker run --rm yen3/test get arm > qemu-arm-static
-chmod +x qemu-arm-static
-docker run -it --rm -v $(pwd)/qemu-arm-static:/usr/local/bin/qemu-arm-static arm32v7/ubuntu uname -a
-docker run --privileged --rm yen3/test clear arm
+    docker run --privileged --rm ${binfmt_image} set ${cpu}
+    docker run --rm ${binfmt_image} get ${cpu} > ${qemu_bin}
+    chmod +x qemu-${cpu}-static
+
+    mount_option="-v $(pwd)/${qemu_bin}:/usr/local/bin/${qemu_bin}"
+}
+
+clear_binfmt() {
+    cpu=$1
+    qemu_bin="qemu-${cpu}-static"
+
+    docker run --privileged --rm ${binfmt_image} clear ${cpu}
+    rm -f ${qemu_bin}
+}
+
+run_uname() {
+    cpu=$1
+    run_image=$2
+
+    [ x"$platform" == x"Linux" ] && set_binfmt $cpu
+
+    docker run -it --rm ${mount_option} ${run_image} uname -a
+
+    [ x"$platform" == x"Linux" ] && clear_binfmt$cpu
+}
+
+check_os() {
+    if [ x"$platform" != x"Linux" ] && [ x"$platform" != x"Darwin" ]; then
+        exit 1
+    fi
+}
+
+build_binfmt_image() {
+    docker build -t ${binfmt_image}  .
+}
+
+build_binfmt_image
+run_uname "arm" "arm32v7"
+run_uname "aarch64" "arm64v8"
